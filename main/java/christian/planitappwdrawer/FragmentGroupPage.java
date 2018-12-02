d74416cef7b0a373f00b6408bbb9d24bee0fe602
cmarks24 <45500939+cmarks24@users.noreply.github.com>
2018-12-01 19:57:00 -0600

    Add files via upload

--- main/java/christian/planitappwdrawer/FragmentGroupPage.java
+++ main/java/christian/planitappwdrawer/FragmentGroupPage.java
@@ -1,6 +1,7 @@
 package christian.planitappwdrawer;
 
 
+import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.app.FragmentTransaction;
 
 import christian.planitappwdrawer.R;
 
@@ -25,7 +27,12 @@ public class FragmentGroupPage extends Fragment {
         View view = inflater.inflate(R.layout.fragment_group_page, container, false);
         btnNavGrpSch = (Button) view.findViewById(R.id.btnNavGrpSch);
 
-
+        btnNavGrpSch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(FragmentGroupPage.this, FragmentGroupSchedule.class));
+            }
+        });
 
         return view;
     }
